5604effa52245aab976e4226d31b20e4cd8c662a
Xan <44590809+x3a1n4@users.noreply.github.com>
2019-11-22 14:32:20 -0500

    Started moving buttons to classes
    Also making the code more class based in general

--- main.js
+++ main.js
@@ -1,15 +1,99 @@
 //code
-clickPowers = [1];
-clickCosts = [10];
-clickCostIncreases = [1.2];
+var clickPowers = [1, 0];
+var clickCosts = [10, 100];
+var clickCostIncreases = [1.2, 2];
+var clickValues = [1, 1];
 
-dripPowers = [0];
-dripCosts = [10];
-dripCostIncreases = [1.2];
+var values = {
+    click: {
+        amounts: [1, 0],
+        costs: [10, 100],
+        increases: [1.2, 2],
+        values: [1, 1]
+    },
 
-number = parseFloat(document.getElementById("number").innerHTML);
+    drip: {
+        amounts: [0, 0],
+        costs: [10, 100],
+        increases: [1.2, 2],
+        values: [1, 1]
+    }
+};
 
-unlockNextThing = 100;
+console.log(values["click"]);
+
+var dripPowers = [0, 0];
+var dripCosts = [10, 100];
+var dripCostIncreases = [1.2, 2];
+var dripValues = [1, 1];
+
+var number = parseFloat(document.getElementById("number").innerHTML);
+
+var unlockNextThing = 100;
+var thingsUnlocked = 1;
+
+String.prototype.title = function(){
+    var string = this; 
+    return(string[0].toUpperCase() +  string.slice(1)); 
+      
+}
+
+class UpgradeButton{
+    constructor(type, level){
+        this.type = type;
+        this.level = level;
+
+        this.id = this.type + "Upgrade" + (this.level - 1);
+    }
+
+    getInnerHTML(){
+        var id = this.level - 1;
+        return(
+                this.type.title() + " lvl " + this.level + 
+                "<hr>" + 
+                Math.ceil(values[this.type]["costs"][id]) + 
+                "<hr>" + 
+                values[this.type]["amounts"][id] + 
+                "<hr>" + 
+                values[this.type]["values"][id]
+            );
+    }
+
+    //this returns the full button element of this class
+    getButton(){
+        var button = document.createElement("BUTTON");
+        var id = this.level - 1;
+
+        button.innerHTML = this.getInnerHTML();
+
+        button.setAttribute("id", this.id);
+        button.setAttribute("onClick", this.type + "Click(" + id + ")");
+
+        return(button);
+    }
+
+    //this actually adds the button to the end of the page
+    makeButton(){
+        var node = document.createElement("TR");
+        
+        node.appendChild(this.getButton());
+        document.getElementById(this.type + "Upgrades").appendChild(node);
+    }
+
+    //this updates the button if values in values are changed
+    updateButton(){
+        document.getElementById(this.id).innerHTML = this.getInnerHTML();
+    }
+}
+
+var buttons = [
+    new UpgradeButton("click", 1),
+    new UpgradeButton("drip", 1)
+];
+
+buttons.forEach(function (item, index) {
+    item.makeButton();
+});
 
 function buttonClick(){
     number += clickPowers[0];
@@ -22,17 +106,16 @@ function dripClick(id){
         dripPowers[id]++;
         dripCosts[id] *= dripCostIncreases[id];
 
-        document.getElementById("dripUpgrade" + id).innerHTML = "Buy drip lvl 1<br>" + Math.ceil(dripCosts[id]);
+        document.getElementById("dripUpgrade" + id).innerHTML = "Buy drip lvl 1<hr>" + Math.ceil(dripCosts[id]) + "<hr>" + dripPowers[id];
     }
 }
-
 function clickClick(id){
     if(number > clickCosts[id]){
         number -= clickCosts[id];
         clickPowers[id]++;
         clickCosts[id] *= clickCostIncreases[id];
 
-        document.getElementById("clickUpgrade" + id).innerHTML = "Buy click lvl 1<br>" + Math.ceil(clickCosts[id]);
+        document.getElementById("clickUpgrade" + id).innerHTML = "Buy click lvl 1<hr>" + Math.ceil(clickCosts[id]) + "<hr>" + clickPowers[id];
     }
 }
 
@@ -41,7 +124,22 @@ function play(){
     number += dripPowers[0] / 20;
 
     if(number > unlockNextThing){
-        unlockNextThing * 10;
+        unlockNextThing *= 10;
+        thingsUnlocked++;
+
+        //add header
+        var node = document.createElement("TH");
+        var textnode = document.createTextNode("Level " + thingsUnlocked);
+        node.appendChild(textnode);
+        document.getElementById("upgradeHeaders").appendChild(node);
+
+        var id = thingsUnlocked - 1;
+
+        //add button
+        node = document.createElement("TR");
+        
+        node.appendChild(button);
+        document.getElementById("clickUpgrades").appendChild(node);
     }
 }
 
